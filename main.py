29ade1bcf242c63f3b1b60f684c05d2f8852ef8a
gomezd11 <gomezd1290@gmail.com>
2023-08-25 08:23:18 -0700

    Assigment 2

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    num_males = 40
+    num_females = 60
 
+    total_students = num_males + num_females
+
+    m_perc = (num_males / total_students) * 100
+    f_perc = (num_females / total_students) * 100
+
+    print("number of students " + str(total_students))
+    print(f"Percentage of males: {m_perc:.2f}%")
+    print(f"Percentage of females: {f_perc:.2f}%")
+    
     """
     ########################################
     # Do not delete the return statement
